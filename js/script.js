fefa11dbef204cec65aa89b05ceef71150dcfef9
Uriel Pereyra <urielinstituto@gmail.com>
2023-08-03 18:38:07 -0300

    Tercera prentrega de mi proyecto

--- js/script.js
+++ js/script.js
@@ -1,66 +1,84 @@
-// Array para almacenar las actividades
-const actividades = [];
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("taskInput");
+    const addButton = document.getElementById("addButton");
+    const taskList = document.getElementById("taskList");
 
-// Función para agregar una actividad
-function agregarActividad(nombre, precioPorMes) {
-    const actividad = { nombre, precioPorMes };
-    actividades.push(actividad);
-}
+    // Obtener tareas del localStorage al cargar la página
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-// Función para calcular el total de las actividades que seleccionaste
-function calcularCostoTotal() {
-    let costoTotal = 0;
-    for (const actividad of actividades) {
-        if (actividad.seleccionada) {
-            costoTotal += actividad.precioPorMes * actividad.meses;
-        }
+    function renderTasks() {
+        taskList.innerHTML = "";
+        tasks.forEach((task, index) => {
+            const listItem = document.createElement("li");
+            listItem.className = "list-group-item d-flex justify-content-between align-items-center";
+
+            const taskLabel = document.createElement("label");
+            taskLabel.className = task.completed ? "form-check-label fw-light text-decoration-line-through" : "form-check-label";
+            taskLabel.innerHTML = `
+            <input type="checkbox" class="form-check-input" ${task.completed ? "checked" : ""} />
+            ${task.title}
+        `;
+
+            const deleteButton = document.createElement("button");
+            deleteButton.className = "btn btn-danger";
+            deleteButton.setAttribute("data-index", index);
+            deleteButton.textContent = "Eliminar";
+
+            listItem.appendChild(taskLabel);
+            listItem.appendChild(deleteButton);
+            taskList.appendChild(listItem);
+        });
     }
-    return costoTotal;
-}
 
-// Función para mostrar las actividades disponibles
-function mostrarActividadesDisponibles() {
-    alert("Actividades disponibles:");
-    actividades.forEach((actividad, index) => {
-        alert(`${index + 1}. ${actividad.nombre} ($${actividad.precioPorMes} por mes)`);
-    });
-}
+    function saveTasksToLocalStorage() {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
 
-// Función para mostrar el resumen de actividades seleccionadas y el costo total en un alert
-function mostrarResumen() {
-    let resumen = "Resumen de actividades:\n";
-    let costoTotal = 0;
+    function addTask() {
+        const taskTitle = taskInput.value.trim();
+        if (taskTitle === "") return;
 
-    actividades.forEach(actividad => {
-        if (actividad.seleccionada) {
-            resumen += `${actividad.nombre} - ${actividad.meses} meses - Costo: $${actividad.precioPorMes * actividad.meses}\n`;
-            costoTotal += actividad.precioPorMes * actividad.meses;
+        const newTask = {
+            title: taskTitle,
+            completed: false,
+        };
+
+        tasks.push(newTask);
+        saveTasksToLocalStorage();
+        renderTasks();
+        taskInput.value = "";
+    }
+
+    function deleteTask(index) {
+        tasks.splice(index, 1);
+        saveTasksToLocalStorage();
+        renderTasks();
+    }
+
+    function toggleTaskCompleted(index) {
+        tasks[index].completed = !tasks[index].completed;
+        saveTasksToLocalStorage();
+        renderTasks();
+    }
+
+    addButton.addEventListener("click", addTask);
+
+    taskInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            addTask();
         }
     });
 
-    resumen += `\nCosto total: $${costoTotal}`;
-    alert(resumen);
-}
+    taskList.addEventListener("click", function (event) {
+        if (event.target.tagName === "BUTTON") {
+            const index = event.target.getAttribute("data-index");
+            deleteTask(index);
+        } else if (event.target.tagName === "INPUT") {
+            const index = event.target.parentNode.getAttribute("data-index");
+            toggleTaskCompleted(index);
+        }
+    });
 
-// Agregar las actividades 
-agregarActividad("Gimnasio", 4000);
-agregarActividad("Natación", 4500);
-agregarActividad("Zumba", 3500);
-agregarActividad("Funcional", 3700);
-
-// Solicitar al usuario que elija las actividades y la cantidad de meses para cada una
-mostrarActividadesDisponibles();
-actividades.forEach(actividad => {
-    const seleccionada = window.confirm(`¿Desea realizar la actividad ${actividad.nombre}?`);
-    if (seleccionada) {
-        const meses = parseInt(prompt(`Ingrese la cantidad de meses que desea realizar ${actividad.nombre}:`));
-        actividad.seleccionada = true;
-        actividad.meses = meses;
-    } else {
-        actividad.seleccionada = false;
-    }
+    // Renderizar las tareas al cargar la página
+    renderTasks();
 });
-
-// Mostrar resumen de actividades y costo total
-mostrarResumen();
-
